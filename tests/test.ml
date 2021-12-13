a9295fc53efac98f5426a0ff207e5aa13172a706
Adrien Guatto <adrien@guatto.org>
2021-12-13 17:25:57 +0100

    Add more tests

--- tests/test.ml
+++ tests/test.ml
@@ -19,33 +19,55 @@ let test_le () =
     let open Term in
 
     let x_id = V.fresh ~name:"x" () in
+    let y_id = V.fresh ~name:"y" () in
     let x = Var x_id in
+    let y = Var y_id in
 
     Problem.[
+        Le (x, x), true;
+        Le (Bot, x), true;
+        Le (x, Top), true;
+
+        (* Congruence-like tests. *)
         Eq (x, x), true;
-        Eq (Comp (x, x), Comp (x, x)), true;
+        Eq (Comp (x, y), Comp (x, y)), true;
+        Eq (Over (x, y), Over (x, y)), true;
+        Eq (Under (x, y), Under (x, y)), true;
+        Eq (Meet (x, y), Meet (x, y)), true;
+        Eq (Join (x, y), Join (x, y)), true;
+
+        (* Universal properties. *)
+        Le (Comp (x, Under (x, y)), y), true;
+        Le (Comp (Over (x, y), y), x), true;
+        Le (x, Under (Comp (x, y), y)), true;
+        Le (y, Under (x, Comp (x, y))), true;
+        Le (Meet (x, y), y), true;
+        Le (Meet (x, y), x), true;
+        Le (x, Join (x, y)), true;
+        Le (y, Join (x, y)), true;
+
+        (* Some trivialy-invalid relations. *)
+        Le (x, y), false;
+        Le (x, Comp (x, x)), false;
+        Le (Comp (x, y), Comp (y, x)), false;
     ]
   in
   List.map
     (fun (prob, is_valid) ->
-      let repr =
-        Printf.sprintf "%s (%svalid)"
-          (Print.to_string @@ Problem.pp prob)
-          (if is_valid then "" else "in")
-      in
+      let repr = Print.to_string @@ Problem.pp prob in
       Alcotest.test_case
         repr
         `Quick
-        (fun () -> Alcotest.check
-                     result
-                     repr
-                     (Problem.to_solution prob)
-                     (if is_valid then `Valid else `Invalid Counterexample.dummy)))
-    cases;;
+        (fun () ->
+          let expected =
+            if is_valid then `Valid else `Invalid Counterexample.dummy
+          in
+          Alcotest.check result repr expected (Problem.to_solution prob)))
+    cases
 
 let () =
   let open Alcotest in
   run "TWMC"
     [
-      "le", test_le ();
+      "validity", test_le ();
     ]
--- twmclib/counterexample.ml
+++ twmclib/counterexample.ml
@@ -4,16 +4,18 @@ type t =
     point : int;
   }
 
-let pp { valuation; point; } =
+let dummy = { valuation = []; point = 0; }
+
+let pp ({ valuation; point; } as cex) =
   let open PPrint in
-  let binding (x, t) = PPrint.prefix 2 1 (Warp.Print.id x) (Compact.pp t) in
-  prefix 2 1 (!^ "values:")
-    (surround_separate_map 2 1
-       (!^ "[]") (!^ "[") (!^ ",") (!^ "]") binding valuation)
-  ^^ prefix 2 1 (!^ "discrepancy:") (!^ (string_of_int point))
+  if cex == dummy then !^ "(dummy)"
+  else
+    let binding (x, t) = PPrint.prefix 2 1 (Warp.Print.id x) (Compact.pp t) in
+    prefix 2 1 (!^ "values:")
+      (surround_separate_map 2 1
+         (!^ "[]") (!^ "[") (!^ ",") (!^ "]") binding valuation)
+    ^^ prefix 2 1 (!^ "discrepancy:") (!^ (string_of_int point))
 
 let equal cex1 cex2 =
   cex1.point = cex2.point
   && Equal.assoc_list (=) Compact.equal cex1.valuation cex2.valuation
-
-let dummy = { valuation = []; point = 0; }
