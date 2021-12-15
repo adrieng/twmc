2df1db7168e99c4644d79c0c464d870032cebdb6
Adrien Guatto <adrien@guatto.org>
2021-12-15 13:10:16 +0100

    Refactor tests and add some

--- tests/test_twmc.ml
+++ tests/test_twmc.ml
@@ -14,60 +14,71 @@ let result =
   in
   Alcotest.testable pp equal
 
-let test_le () =
-  let cases =
-    let open Term in
+let test_case_of_problem expected prob =
+  let repr = Print.to_string @@ Problem.pp prob in
+  Alcotest.test_case
+    repr
+    `Quick
+    (fun () ->
+      Alcotest.check result repr expected (Problem.to_solution prob))
 
-    let x_id = V.fresh ~name:"x" () in
-    let y_id = V.fresh ~name:"y" () in
-    let x = Var x_id in
-    let y = Var y_id in
+let x_id = Term.V.fresh ~name:"x" ()
+let y_id = Term.V.fresh ~name:"y" ()
+let z_id = Term.V.fresh ~name:"z" ()
+let x, y, z = Term.(Var x_id, Var y_id, Var z_id)
 
-    Problem.[
-        Le (x, x), true;
-        Le (Bot, x), true;
-        Le (x, Top), true;
-
-        (* Congruence-like tests. *)
-        Eq (x, x), true;
-        Eq (Comp (x, y), Comp (x, y)), true;
-        Eq (Over (x, y), Over (x, y)), true;
-        Eq (Under (x, y), Under (x, y)), true;
-        Eq (Meet (x, y), Meet (x, y)), true;
-        Eq (Join (x, y), Join (x, y)), true;
-
-        (* Universal properties. *)
-        Le (Comp (x, Under (x, y)), y), true;
-        Le (Comp (Over (x, y), y), x), true;
-        Le (x, Under (Comp (x, y), y)), true;
-        Le (y, Under (x, Comp (x, y))), true;
-        Le (Meet (x, y), y), true;
-        Le (Meet (x, y), x), true;
-        Le (x, Join (x, y)), true;
-        Le (y, Join (x, y)), true;
-
-        (* Some trivialy-invalid relations. *)
-        Le (x, y), false;
-        Le (x, Comp (x, x)), false;
-        Le (Comp (x, y), Comp (y, x)), false;
-    ]
-  in
+let test_valid () =
+  let open Term in
+  let open Problem in
   List.map
-    (fun (prob, is_valid) ->
-      let repr = Print.to_string @@ Problem.pp prob in
-      Alcotest.test_case
-        repr
-        `Quick
-        (fun () ->
-          let expected =
-            if is_valid then `Valid else `Invalid Counterexample.dummy
-          in
-          Alcotest.check result repr expected (Problem.to_solution prob)))
-    cases
+    (test_case_of_problem `Valid)
+    [
+      (* Congruence-like tests. *)
+      Eq (x, x);
+      Eq (Comp (x, y), Comp (x, y));
+      Eq (Over (x, y), Over (x, y));
+      Eq (Under (x, y), Under (x, y));
+      Eq (Meet (x, y), Meet (x, y));
+      Eq (Join (x, y), Join (x, y));
+
+      (* Universal properties. *)
+      Le (Comp (x, Under (x, y)), y);
+      Le (Comp (Over (x, y), y), x);
+      Le (x, Under (Comp (x, y), y));
+      Le (y, Under (x, Comp (x, y)));
+      Le (Bot, x);
+      Le (Meet (x, y), y);
+      Le (Meet (x, y), x);
+      Le (x, Top);
+      Le (x, Join (x, y));
+      Le (y, Join (x, y));
+
+      (* Lattice properties. *)
+      Eq (Meet (x, Top), x);
+      Eq (Meet (x, Meet (y, z)), Meet (Meet (x, y), z));
+      Eq (Meet (x, y), Meet (y, x));
+      Eq (Meet (x, x), x);
+      Eq (Join (x, Bot), x);
+      Eq (Join (x, Join (y, z)), Join (Join (x, y), z));
+      Eq (Join (x, y), Join (y, x));
+      Eq (Join (x, x), x);
+    ]
+
+let test_invalid () =
+  let open Term in
+  let open Problem in
+  List.map
+    (test_case_of_problem (`Invalid Counterexample.dummy))
+    [
+      Le (x, y);
+      Le (x, Comp (x, x));
+      Le (Comp (x, y), Comp (y, x));
+    ]
 
 let () =
   let open Alcotest in
   run "TWMC"
     [
-      "validity", test_le ();
+      "validity", test_valid ();
+      "invalidity", test_invalid ();
     ]
