e6396d67dd991cc4668daf13f48434f2153143d4
Adrien Guatto <adrien@guatto.org>
2022-12-03 13:00:08 +0100

    Make comments in .smt files more informative

--- twmclib/diagram.ml
+++ twmclib/diagram.ml
@@ -80,23 +80,23 @@ let translate
             (fun a2 ->
               if not (Sample.equal a1 a2)
               then holds
-                     ~comment:"3.1"
+                     ~comment:"3.1: a1 <= a2 => t[a1] <= t[a2]"
                      ((d a1 <= d a2) ==> ((deval t a1) <= (deval t a2)))));
       for_all
         (fun a ->
           (* Condition 3.2. *)
           holds
-            ~comment:"3.2"
+            ~comment:"3.2: a = 0 => t[a] = 0"
             (is_zero (d a) ==> is_zero (deval t a));
           (* Condition 3.4. *)
           holds
-            ~comment:"3.4"
+            ~comment:"3.4: last t <= a => t[last t] = t[a]"
             (dlast t <= d a <==> (devallast t = deval t a));
           (* Condition 3.5. *)
           begin match Sample.view a with
           | Sample.SLast t' when Basic.equal t t' ->
              holds
-                ~comment:"3.5"
+               ~comment:"3.5: last t = omega => t[last t] = omega"
                (is_omega (d a) ==> is_omega (devallast t))
           | _ ->
              ()
@@ -108,7 +108,9 @@ let translate
         (fun a ->
           match Sample.view a with
           | Sample.SSucc a' ->
-             holds ~comment:"3.3" @@ is_succ ~pre:(d a') ~suc:(d a)
+             holds
+               ~comment:"3.3: succ a' = S a"
+               (is_succ ~pre:(d a') ~suc:(d a))
 
           | _ ->
              ())
@@ -119,30 +121,42 @@ let translate
       | Id ->
          comment "* Identity conditions *";
          (* Condition 3.6. *)
-         holds_for_all ~comment:"3.6" (fun a -> deval t a = d a);
+         holds_for_all
+           ~comment:"3.6: id[a] = a"
+           (fun a -> deval t a = d a);
          (* Condition 3.7 *)
-         holds ~comment:"3.7" @@ is_omega (dlast t)
+         holds
+           ~comment:"3.7: last id = omega"
+         @@ is_omega (dlast t)
+
       | Comp (u, v) ->
          comment "* Composition conditions *";
          (* Condition 3.8. *)
-         holds_for_all ~comment:"3.8"
+         holds_for_all ~comment:"3.8: u[v[a]] = uv[a]"
            (fun a -> deval u (Sample.eval v a) = deval t a);
          (* Condition 3.9. *)
-         holds ~comment:"3.9" @@ (is_omega (dlast t) ==>
-                                    (is_omega (dlast u) && is_omega (dlast v)))
+         holds
+           ~comment:"3.9: last uv = omega => last u = omega && last v = omega"
+           (is_omega (dlast t) ==> (is_omega (dlast u) && is_omega (dlast v)))
+
       | Neg u ->
          comment "* Negation conditions *";
          (* Condition 3.10. *)
-         holds_for_all ~comment:"3.10"
+         holds_for_all
+           ~comment:"3.10: u[u'[a]] < a"
            (fun a -> is_finite_positive (d a) ==>
                        (deval u (Sample.eval t a) < d a));
          (* Condition 3.11. *)
-         holds_for_all ~comment:"3.11"
+         holds_for_all
+           ~comment:"3.11: u'[a] < omega => a <= u[S[u'[a]]"
            (fun a -> is_finite (deval t a) ==>
                        (d a <= deval u Sample.(succ (eval t a))));
          (* Condition 3.12. *)
-         holds ~comment:"3.12" @@ (is_omega (dlast t) ==> is_omega (dlast u))
-      | _ ->
+         holds
+           ~comment:"3.12: last u' = omega => last u = omega"
+         @@ (is_omega (dlast t) ==> is_omega (dlast u))
+
+      | Var _ ->
          ()
       end
     )
